c17effc8ac3f33b618bb400897a53bb391a3a70a
Roshan Gokul <roshangokul@gmail.com>
2024-06-16 01:48:01 -0500

    NavBar is completed, finish up about and set up projects and experience

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import NavBar from './NavBar';
+import About from './About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <About />
     
   </React.StrictMode>
 );
