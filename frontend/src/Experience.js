fed813229ec0183f53580fb0a20f4769ba6791a1
roshangokul <roshangokul@gmail.com>
2024-12-30 19:11:35 -0800

    set boilerplate for timeline

--- frontend/src/Experience.js
+++ frontend/src/Experience.js
@@ -1,35 +1,94 @@
 import React from 'react';
 import './Experience.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 
 const Experience = () => {
   return (
-    <div className="experience-section">
-          <section id="experience-section">
-          
-      <h2 className="section-title">My Journey</h2>
-      <div className="experience-scroll">
-        <div className="experience-item">
-          <h3>Software Engineer Intern</h3>
-          <p><b>Company:</b> UnitedHealth Group</p>
-          <p><b>Date:</b> 2023 - Present</p>
-          <p>
-            Worked on optimizing workflows, improving code efficiency, and 
-            developing real-time data monitoring systems.
-          </p>
-        </div>
-        <div className="experience-item">
-          <h3>Bachelor of Science</h3>
-          <p><b>Institution:</b> University of Minnesota, Twin-Cities</p>
-          <p><b>Date:</b> 2020 - 2023</p>
-          <p>
-            Majored in Computer Science with a focus on software engineering 
-            and full-stack development.
-          </p>
-        </div>
-        {/* Add more experience items as needed */}
-      </div>
-      </section>
-    </div>
+    <div className='experience'>
+      <section id="experience-section"></section>
+    <VerticalTimeline>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+    date="2011 - present"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    // icon={<WorkIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Incoming Software Engineer @ Chewy</h3>
+    <h4 className="vertical-timeline-element-subtitle">Bellevue, WA</h4>
+    <p>
+      TBD
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    date="2010 - 2011"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    // icon={<WorkIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Software Engineer Co-Op</h3>
+    <h4 className="vertical-timeline-element-subtitle">Minneapolis, MN & Bellevue, WA</h4>
+    <p>
+      I worked on the Order Release & Customization Service team in the Supply Chain Org. I was able to work with and gain a deeper knowledge of AWS technologies like S3, SNS, SQS, MSK, EC2, and Cloudwatch. I worked primarily in Java using Spring Boot, but also worked with Apache Kafka, Python, PostgresSQL, Docker, Git and Jenkins for development and deployment, and Cucumber and Gherkin for testing.
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    date="2006 - 2008"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    // icon={<WorkIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Software Development Intern @ ImageTrend</h3>
+    <h4 className="vertical-timeline-element-subtitle">Greater Minneapolis-St. Paul Area</h4>
+    <p>
+      I worked on the Elite Team and I utilized the C#/.NET framework to create an in-house application to automatically assign PR reviews to team members in office, taking into account substantial time dedicated to meetings, partial time-off during the day, and OOO
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="April 2013"
+    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    // icon={<SchoolIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+    <p>
+      Strategy, Social Media
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="November 2012"
+    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    // icon={<SchoolIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+    <p>
+      Creative Direction, User Experience, Visual Design
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="2002 - 2006"
+    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    // icon={<SchoolIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+    <p>
+      Creative Direction, Visual Design
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+    // icon={<StarIcon />}
+  />
+</VerticalTimeline>
+</div>
   );
 };
 
