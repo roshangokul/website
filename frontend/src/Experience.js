3140e29c1b441adf6053e713411c321d30c8e35d
roshangokul <roshangokul@gmail.com>
2024-12-30 23:48:16 -0800

    modified some changes to my layout, still need to make about me more responsive

--- frontend/src/Experience.js
+++ frontend/src/Experience.js
@@ -8,12 +8,13 @@ const Experience = () => {
   return (
     <div className='experience'>
       <section id="experience-section"></section>
+      <h1 className='experience-header'>My Journey</h1>
     <VerticalTimeline>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
     contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="2011 - present"
+    date="July 2025"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     // icon={<WorkIcon />}
   >
@@ -25,11 +26,11 @@ const Experience = () => {
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    date="2010 - 2011"
+    date="May 2024 - Dec 2024"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     // icon={<WorkIcon />}
   >
-    <h3 className="vertical-timeline-element-title">Software Engineer Co-Op</h3>
+    <h3 className="vertical-timeline-element-title">Software Engineer Co-Op @ Chewy</h3>
     <h4 className="vertical-timeline-element-subtitle">Minneapolis, MN & Bellevue, WA</h4>
     <p>
       I worked on the Order Release & Customization Service team in the Supply Chain Org. I was able to work with and gain a deeper knowledge of AWS technologies like S3, SNS, SQS, MSK, EC2, and Cloudwatch. I worked primarily in Java using Spring Boot, but also worked with Apache Kafka, Python, PostgresSQL, Docker, Git and Jenkins for development and deployment, and Cucumber and Gherkin for testing.
@@ -37,7 +38,7 @@ const Experience = () => {
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    date="2006 - 2008"
+    date="May 2023 - Aug 2023"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     // icon={<WorkIcon />}
   >
@@ -49,43 +50,20 @@ const Experience = () => {
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--education"
-    date="April 2013"
+    date="2023-2025"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    // icon={<SchoolIcon />}
   >
-    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+    <h3 className="vertical-timeline-element-title">Undergraduate Teaching Assistant</h3>
     <p>
-      Strategy, Social Media
+        <ul>CSCI 1933 - Introduction to Data Structures and Alorithms</ul>
+        <ul>CSCI 2033 - Computational Linear Algebra</ul>
+        <ul>CSCI 1133 - Introduction to Computing and Programming Concepts</ul>
     </p>
   </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="November 2012"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    // icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-    <p>
-      Creative Direction, User Experience, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="2002 - 2006"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    // icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-    <p>
-      Creative Direction, Visual Design
-    </p>
-  </VerticalTimelineElement>
+ 
+  
   <VerticalTimelineElement
     iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-    // icon={<StarIcon />}
   />
 </VerticalTimeline>
 </div>
