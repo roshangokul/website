004024ad4b1d9ef9940046ac33607c3c879a7e0f
roshangokul <roshangokul@gmail.com>
2025-07-22 21:53:09 -0700

    Updated app with new font and mobile changes

---
+++ src/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Projects.css';
+
+const Projects = () => {
+  return (
+    <div className='projects'>
+      <section id="projects"></section>
+        <div>
+        {/* <h1>Projects</h1> */}
+        <p>Currently adding this section</p>
+
+        </div>
+    </div>
+  );
+}
+
+export default Projects;
